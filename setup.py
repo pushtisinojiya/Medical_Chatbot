c698e4b57ce542523ad67a484e3074899c8e035c
Pushti Sinojiya <sinojiyapushti@gmail.com>
2026-06-25 04:43:39 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+setup(
+    name="Medical_Chatbot",
+    version="0.1.0",
+    author="Pushti Sinojiya",
+    author_email="sinojiyapushti@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
